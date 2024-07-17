a66d7e8a8c37cbaa6ab7560a855d5c4f508a25c5
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-17 09:52:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/AST.fs
+++ src/AST.fs
@@ -3,7 +3,13 @@
 
 module AST
 
+/// language expression
 type Expr =
+    /// integer
     | Int of int
+    /// string
     | Str of string
+    /// variable
     | Var of string
+    /// binary operator
+    | BinOp of string * Expr * Expr
