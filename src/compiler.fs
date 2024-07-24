36663c5fcbd09c72ee3414ee13912a31ab10c8f4
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-24 17:00:59 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/compiler.fs
+++ src/compiler.fs
@@ -5,25 +5,41 @@ module compiler
 
 open AST
 
-type ByteCode =
-    // single-path sequence
-    | Seq of ByteCode array
+type VMCmd =
     // control
-    | Nop
-    | Halt
-    // math
-    | Add
-    | Sub
-    | Mul
-    | Div
+    /// `( -- )` do nothing
+    | Nop = 0x00uy
+    /// `( -- )` stop system
+    | Halt = 0xFFuy
     // stack
-    | Dup
-    | Drop
-    | Swap
-    | Over
+    /// `( n -- n n )`
+    | Dup = 0x10uy
+    /// `( n1 n2 -- n1 )`
+    | Drop = 0x11uy
+    /// `( n1 n2 -- n2 n1 )`
+    | Swap = 0x12uy
+    /// `( n1 n2 -- n1 n2 n1 )`
+    | Over = 0x13uy
+    // math
+    /// `( n1 n2 -- n1+n2 )` sum
+    | Add = 0x20uy
+    /// `( n1 n2 -- n1-n2 )` subtract
+    | Sub = 0x21uy
+    /// `( n1 n2 -- n1*n2 )` multiply
+    | Mul = 0x22uy
+    /// `( n1 n2 -- n1/n2 )` integer division
+    | Div = 0x23uy
+
+type ByteCode =
+    /// single command
+    | Cmd of VMCmd
+    /// single-path block
+    | Block of ByteCode array
+
+let dumblock = Block([| Cmd VMCmd.Nop; Cmd VMCmd.Halt |])
 
 let test = //
-    printfn "%A" (Seq([| Nop; Halt |]))
+    assert ($"{dumblock}" = "Block [|Cmd Nop; Cmd Halt|]")
 
 [<EntryPoint>]
 let main (argv: string array) =
