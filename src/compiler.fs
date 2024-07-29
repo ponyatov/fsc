2feec6c7afd2b9978f834f9cac2af97f5c8088c5
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-29 12:53:13 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/compiler.fs
+++ src/compiler.fs
@@ -5,18 +5,25 @@ module compiler
 
 open AST
 
+/// command with argument
+type Cmd1 =
+    // control
+    /// `( -- )` unconditional jump
+    | Jmp = 0x01uy
+    /// `( zero -- )` jump if zero
+    | qJmp = 0x02uy
+    /// `R:( -- addr )` nested call
+    | Call = 0x03uy
+    /// `( -- n )` literal
+    | Lit = 0x05uy
+
+/// no-arg commands
 type Cmd0 =
     // control
     /// `( -- )` do nothing
     | Nop = 0x00uy
     /// `( -- )` stop system
     | Halt = 0xFFuy
-    /// `( -- )` unconditional jump
-    |Jmp = 0x01uy
-    /// `( zero -- )` jump if zero
-    |Jz = 0x02uy
-    /// `R:( -- addr )` nested call
-    | Call = 0x03uy
     /// `R:( addr -- )` return from nested call
     | Ret = 0x04uy
     // stack
@@ -28,6 +35,14 @@ type Cmd0 =
     | Swap = 0x12uy
     /// `( n1 n2 -- n1 n2 n1 )`
     | Over = 0x13uy
+    /// `( n1 n2 n3 -- n2 n3 n1)`
+    | Rot = 0x14uy
+    /// `( n1 n2 n3 -- n3 n1 n2 )`
+    | mRot = 0x15uy
+    /// `( ... i -- ... ni)
+    | Pick = 0x16uy
+    /// `( ... -- ... n ) stack depth
+    | Depth = 0x17uy
     // math
     /// `( n1 n2 -- n1+n2 )` sum
     | Add = 0x20uy
@@ -37,6 +52,22 @@ type Cmd0 =
     | Mul = 0x22uy
     /// `( n1 n2 -- n1/n2 )` integer division
     | Div = 0x23uy
+    /// `( n1 n2 -- n1 % n2 )`
+    | Mod = 0x24uy
+    /// bit operators
+    /// `( n -- !n)` invert bits
+    | Not = 0x25uy
+    /// `( n1 n2 -- n1|n2 )`
+    | Or = 0x26uy
+    /// `( n1 n2 -- n1&n2 )`
+    | And = 0x27uy
+    /// `( n1 n2 -- n1^n2 )`
+    | Xor = 0x28uy
+
+    /// `( n1 n2 -- n1 << n2 )` bit shift left
+    | Shl = 0x29uy
+    /// `( n1 n2 -- n1 >> n2 )` bit shift right
+    | Shr = 0x2Auy
 
 type ByteCode =
     /// single command
