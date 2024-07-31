a12c3fe93efb358a7ceac7727bbc20a238183042
Dmitry Ponyatov <dponyatov@gmail.com>
2024-07-31 10:13:47 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/Cpp.fs
+++ src/Cpp.fs
@@ -1,17 +1,35 @@
+/// formatting attributes
+type A =
+    | s of string
+    | b of (string*bool)
+    | i of (string*int)
+
+/// generic Source code repr
 type S =
-    | bare of string
-    | obj  of {
-            pfx: optional S
-            nest: S list
-            sfx: optional S
+    type S' =
+        | bare of string
+        | lst  of string list
+    | tree of {
+            attr: A  set
+            pfx:  S' optional
+            nest: S
+            sfx:  S' optional
         }
 
 sigil C"s" = Cpp.parser s
 
 const tabsize = 4
 
-let dump (src:S) (depth: int = 0): string =
+let dump (src:S) (depth: int = 0): string = seq {
     match S with
-    | S.bare s -> (String.replicate (depth*tabsize) " ") @ s
-    | S.obj pfx nest sfx ->
-        
+    | S.bare s ->
+        String.replicate (depth*tabsize) " " @ s
+    | S.list l ->
+        yield! List map l {s -> dump s depth}
+    | S.tree attr pfx nest sfx ->
+        yield! preDump pfx  (depth+0)
+        yield! preDump nest (depth+1)
+        yield! preDump sfx  (depth+0)
+}
+
+let write file src = file |> File.WriteAllLines (dump src 0)
